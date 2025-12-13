cb015b591cf79f92b4c0e6a402973abccaba8a20
Stefan Nedelcu <stefan.n@gmx.co.uk>
2025-12-13 20:20:11 +0000

    Added shooting stars background to home page

---
+++ app/lib/utils.ts
@@ -0,0 +1,7 @@
+// credits to aceternity UI
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
